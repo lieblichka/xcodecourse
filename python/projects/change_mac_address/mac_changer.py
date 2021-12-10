ee6234de6ccb6bca12dfdbd227f62a0a1332dd61
mwuckert <nesteatea1212@gmail.com>
2021-12-10 20:37:23 +0300

    projects/change_mac/*

--- python/projects/change_mac_address/mac_changer.py
+++ python/projects/change_mac_address/mac_changer.py
@@ -2,4 +2,8 @@
 
 import subprocess # подключение модуля subprocess
 
-subprocess.call("ifconfig", shell=True)
+subprocess.call("ifconfig wlp3s0 down", shell=True)
+subprocess.call("ifconfig wlp3s0 hw ether 50:b7:c3:e1:ba:a5",
+				 shell=True)
+subprocess.call("ifconfig wlp3s0 up", shell=True)
+
