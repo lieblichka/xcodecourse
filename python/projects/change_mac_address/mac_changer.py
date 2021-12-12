36f2e16ed0e2492b618c5fe017d1d2ab0f5c2b11
mwuckert <nesteatea1212@gmail.com>
2021-12-12 19:47:56 +0300

    n img/*.jpg, m mac_changer.py, mans/change_mac_address_python.txt

--- python/projects/change_mac_address/mac_changer.py
+++ python/projects/change_mac_address/mac_changer.py
@@ -2,7 +2,7 @@
 
 import subprocess # подключение модуля subprocess
 import optparse  # модуль для продвинутого парсинга
-
+import re # модуль регулярных выражений
 
 def get_arguments(): # объявление функции
 	parser = optparse.OptionParser() 
@@ -31,11 +31,23 @@ def change_mac(interface, new_mac):
 	subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
 	subprocess.call(["ifconfig", interface, "up"])
 
+def get_current_mac(interface):
+	ifconfig_result = subprocess.check_output(["ifconfig", interface],
+	universal_newlines=True)
+	# сохранение результата вывода функции в переменную
+
+	#print(ifconfig_result) # вывод значения перменной на экран
+	current_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+	# re.search - метод поиска совпадений по регулярному выражению 
+	if current_mac:
+		return current_mac.group(0)
+	return None
+
 
 options = get_arguments()
-#change_mac(options.interface, options.new_mac)
-ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
-# сохранение результата вывода функции в переменную
-
-print(ifconfig_resul) # вывод значения перменной на экран
-
+change_mac(options.interface, options.new_mac)
+mac_address = get_current_mac(options.interface)
+if mac_address:
+	print ("Current mac address: " + mac_address)
+else:
+	print ("[-] Could not read mac address")
